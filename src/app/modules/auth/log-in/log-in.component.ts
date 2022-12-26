6b86d9b0f04c9ec260201f19a431ea18efaa9564
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2022-12-27 00:44:34 +0530

    Created Login form

--- src/app/modules/auth/log-in/log-in.component.ts
+++ src/app/modules/auth/log-in/log-in.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-log-in',
-  templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.scss']
+  selector: "app-log-in",
+  templateUrl: "./log-in.component.html",
+  styleUrls: ["./log-in.component.scss"]
 })
 export class LogInComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  loginForm = new FormGroup({
+    username: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required)
+  });
+
+  logIn() {
+    console.log(this.loginForm.value);
+  }
 }
