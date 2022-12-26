8355eac28341a8b0bee46bfb4dc4d881f0a6c053
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2022-12-27 00:06:00 +0530

    Created auth,layout,task module and header,sidebar,login,signup component

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LogInComponent } from './log-in/log-in.component';
+
+
+@NgModule({
+  declarations: [
+    SignUpComponent,
+    LogInComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
---
+++ src/app/modules/layout/layout.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LayoutComponent } from './layout.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    LayoutComponent
+  ],
+  exports: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule
+  ]
+})
+export class LayoutModule { }
