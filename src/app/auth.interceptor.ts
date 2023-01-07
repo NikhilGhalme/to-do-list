c7ab603d64c0b57d15aa8826f0addafd47d8783a
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-01-07 11:22:27 +0530

    Handle 401 error with appropriate popup

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,24 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+  HttpInterceptor, HttpErrorResponse
+} from "@angular/common/http";
+import * as Notiflix from "notiflix";
+import { Notify } from "notiflix";
+import { Observable, tap } from "rxjs";
+
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() {
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const token = localStorage.getItem('token');
-    if(token) {
+    const token = localStorage.getItem("token");
+    if (token) {
       request = request.clone({
         setHeaders: {Authorization: `Bearer ${token}`}
       });
     }
-    return next.handle(request);
+    return next.handle(request).pipe(tap(() => {
+    }, error => {
+      if (error instanceof HttpErrorResponse && error.status === 401) {
+        Notify.failure("Invalid credentials", {
+          position: "center-top",
+          fontSize: "16px",
+        });
+      }
+    }));
   }
 }
