0a2a3f0828dad646e93ae89c5a9943fcb0851ab1
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-03-08 23:47:34 +0530

    Added Validetaion for header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-list';
+  isLogIn = false;
+
+  constructor(){
+    const token = localStorage.getItem("token");
+    if(token){
+      this.isLogIn = true;
+    }
+  }
 }
