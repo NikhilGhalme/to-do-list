bfe46deb41e668ed7179507d9c5a3d5a1af7eef5
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-04-17 00:48:03 +0530

    small reformating

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,33 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  {
-    path: "auth",
-    loadChildren: () => import("./modules/auth/auth.module").then(m => m.AuthModule),
-    title: "To Do List Authentication",
-  },
-  {
-    path: "dashboard",
-    loadChildren: () => import("./modules/layout/layout.module").then(m => m.LayoutModule),
-    title: "ToDoList | Profile",
-  },
-  {
-    path: "task",
-    loadChildren: () => import("./modules/task/task.module").then(m => m.TaskModule),
-    title: "ToDoList | Task Management",
-  },
-  {
-    path: "",
-    redirectTo: "/auth/login",
-    pathMatch: "full",
-  },
-  { path: "**", redirectTo: "/errors/404" },
+	{
+		path: "auth",
+		loadChildren: () => import("./modules/auth/auth.module").then(m => m.AuthModule),
+		title: "To Do List Authentication",
+	},
+	{
+		path: "dashboard",
+		loadChildren: () => import("./modules/layout/layout.module").then(m => m.LayoutModule),
+		title: "ToDoList | Profile",
+	},
+	{
+		path: "task",
+		loadChildren: () => import("./modules/task/task.module").then(m => m.TaskModule),
+		title: "ToDoList | Workspace Management",
+	},
+	{
+		path: "",
+		redirectTo: "/auth/login",
+		pathMatch: "full",
+	},
+	{path: "**", redirectTo: "/errors/404"},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule {
 
