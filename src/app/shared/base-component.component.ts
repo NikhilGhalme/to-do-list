d811fa62046c13c60da8f4f564581ca2d0cf66bf
Vishwajit Satish Sayambar <vishwajitsayambar@gmail.com>
2023-04-28 00:55:56 +0530

    FIX: UI issues for mobile View

--- src/app/shared/base-component.component.ts
+++ src/app/shared/base-component.component.ts
@@ -6,10 +6,12 @@ export abstract class BaseComponent {
    token: string | null;
    notify:any;
    router:Router;
+    isMobileDevice = window.innerWidth < 720;
   protected constructor(injector: Injector) {
       this.router = injector.get(Router);
   this.token = localStorage.getItem('token');
   this.notify = Notiflix;
   }
+
 
 }
